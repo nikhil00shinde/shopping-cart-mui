fa81c012b89ddbf4ed324a3bc682582bb85e8d53
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-02-09 21:12:36 +0530

    Updates

---
+++ precache-manifest.c3f8e5cab422448f8d57431e71b91558.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd91f71a13b8e8f2ed5cdc8f9fb0b53",
+    "url": "/shopping-cart-mui/index.html"
+  },
+  {
+    "revision": "9f75fb169696a78e2ab1",
+    "url": "/shopping-cart-mui/static/css/main.688a5871.chunk.css"
+  },
+  {
+    "revision": "32b650c25670705f154e",
+    "url": "/shopping-cart-mui/static/js/2.7124a6e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shopping-cart-mui/static/js/2.7124a6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f75fb169696a78e2ab1",
+    "url": "/shopping-cart-mui/static/js/main.d79b970c.chunk.js"
+  },
+  {
+    "revision": "f46f3e7bf26b4a5addb7",
+    "url": "/shopping-cart-mui/static/js/runtime-main.7c067a98.js"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/shopping-cart-mui/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/shopping-cart-mui/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/shopping-cart-mui/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
